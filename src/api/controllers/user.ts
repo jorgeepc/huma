cc90d72b3e611a918af75a39dd3d73c79c1c88d1
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2019-09-05 16:14:59 -0500

    Add HAL support

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
+import halson from 'halson'
 import format from '../../utils/format'
 import Logger from '../../utils/logger'
 
 export const getUser = (req: Request, res: Response, next: NextFunction) => {
   const username = req.params.username
-  Logger.info(username)
-  return format(res, { username }, 200)
+  const user = halson({ username }).addLink('self', `/users/${username}`)
+  Logger.info(JSON.stringify(user))
+  return format(res, user, 200)
 }
